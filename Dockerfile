2d78a31f40fc9d5404355858233d8fbb67612902
betacode-projects <betacode-projects.net@gmail.com>
2022-05-12 00:32:51 +0900

    swagger導入

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,6 @@ WORKDIR /app
 COPY ./app /app
 
 RUN go install github.com/cosmtrek/air@latest
+RUN go install github.com/swaggo/swag/cmd/swag@latest
 
 EXPOSE 8080
--- app/main.go
+++ app/main.go
@@ -3,11 +3,25 @@ package main
 import (
 	"net/http"
 
+	_ "main/docs"
+
+	echoSwagger "github.com/swaggo/echo-swagger"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 var e = createMux()
+
+// @title Shakyou API
+// @version 1.0
+// @description S先生の写経課題を楽に終わらせるためのAPI
+
+// @contact.name hirosuke-pi
+// @contact.url https://www.twitter.com/hirosuke_pi
+
+// @host localhost:8080
+// @BasePath /api/v1
 
 func main() {
 	e.GET("/", articleIndex)
@@ -21,6 +35,8 @@ func createMux() *echo.Echo {
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
 	e.Use(middleware.Gzip())
+
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	return e
 }
