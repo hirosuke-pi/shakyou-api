5e035cb09b6747681372e69353beb45291880a5b
betacode-projects <betacode-projects.net@gmail.com>
2022-05-19 00:04:46 +0900

    コード抽出完成

--- app/mods/pdf2code.go
+++ app/mods/pdf2code.go
@@ -5,52 +5,59 @@ import (
 	"log"
 	"os"
 	"regexp"
-	"strconv"
 	"strings"
 
 	"code.sajari.com/docconv"
 )
 
-func Pdf2Code(input string, output string) (string, error) {
-	testCode()
-
-	return "", nil
-}
-
-type CodeFiles struct {
+type FileConfig struct {
 	codes []string
 	path  string
+	name  string
 	nest  bool
 }
 
-func testCode() {
-	for _, v := range []string{"2022ST42AndroidSample01.pdf"} { //, "2022ST42AndroidSample02.pdf", "2022ST42AndroidSample03.pdf"} {
-		res, err := docconv.ConvertPath(v)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
+func Pdf2Code(input string, output string) ([]FileConfig, error) {
+	input = "2022ST42AndroidSample01.pdf"
 
-		bodyLines := regexp.MustCompile("\r\n|\n").Split(res.Body, -1)
-		files := []CodeFiles{}
-
-		// コードを分割
-		extractCode(&files, &bodyLines)
-
-		fmt.Println(v + ".txt")
-		outputCode(strings.Join(bodyLines, "\n"), v+"_.txt")
-		outputCode(res.Body, v+".txt")
+	// pdfから文字を抽出
+	res, err := docconv.ConvertPath(input)
+	if err != nil {
+		log.Println(err)
+		return nil, err
 	}
+
+	// 初期設定
+	outputFile(res.Body, input+".txt")
+	files := extractPdf(res.Body)
+
+	fmt.Println(input + ".txt")
+
+	return files, nil
 }
 
-func extractCode(files *[]CodeFiles, bodyLines *[]string) {
+func extractPdf(body string) []FileConfig {
+	bodyLines := regexp.MustCompile("\r\n|\n").Split(body, -1)
+
+	files := extractCode(&bodyLines) // コード部分を抽出
+	extractName(&files, &bodyLines)  // ファイル名を抽出
+	extractNest(&files, &bodyLines)  // ネストされたファイルかどうか
+	joinNestedFile(&files)           // ネストされていた場合、結合する
+
+	for _, v := range files {
+		outputFileLines(v.codes, `./archives/`+v.name)
+	}
+	return files
+}
+
+func extractCode(bodyLines *[]string) []FileConfig {
+	files := []FileConfig{}
 	codes := []string{}
 	expCodes := []string{}
-	tmpLines := *bodyLines
 	codeFlag := false
 	newlineFlag := false
 
-	for i, v := range tmpLines {
+	for _, v := range *bodyLines {
 		if regexp.MustCompile(".*?¬").MatchString(v) {
 			line := strings.ReplaceAll(v, "¬", "")
 
@@ -65,11 +72,7 @@ func extractCode(files *[]CodeFiles, bodyLines *[]string) {
 			codes[len(codes)-1] += v
 			newlineFlag = true
 		} else if codeFlag {
-			*files = append(*files, CodeFiles{
-				codes: codes,
-			})
-			outputCode(strings.Join(codes, "\n"), strconv.Itoa(i)+".txt")
-
+			files = append(files, FileConfig{codes: codes})
 			codeFlag = false
 			codes = []string{}
 		} else {
@@ -78,15 +81,63 @@ func extractCode(files *[]CodeFiles, bodyLines *[]string) {
 	}
 
 	if codeFlag {
-		*files = append(*files, CodeFiles{
-			codes: codes,
-		})
-		outputCode(strings.Join(codes, "\n"), "999999999.txt")
+		files = append(files, FileConfig{codes: codes})
 	}
 	*bodyLines = expCodes
+
+	return files
 }
 
-func outputCode(body string, path string) {
+func extractName(files *[]FileConfig, bodyLines *[]string) {
+	index := 0
+	expNames := []string{}
+
+	for i, v := range *bodyLines {
+		if regexp.MustCompile("^Printed:.*?").MatchString(v) {
+			(*files)[index].name = (*bodyLines)[i-1]
+			expNames = expNames[:len(expNames)-1]
+			index++
+		} else {
+			expNames = append(expNames, v)
+		}
+	}
+	*bodyLines = expNames
+}
+
+func extractNest(files *[]FileConfig, bodyLines *[]string) {
+	index := 0
+	expNames := []string{}
+
+	for _, v := range *bodyLines {
+		if regexp.MustCompile(`^Page 1/\d+?$`).MatchString(v) {
+			(*files)[index].nest = false
+			index++
+		} else if regexp.MustCompile(`^Page \d+?/\d+?$`).MatchString(v) {
+			(*files)[index].nest = true
+			index++
+		} else {
+			expNames = append(expNames, v)
+		}
+	}
+	*bodyLines = expNames
+}
+
+func joinNestedFile(files *[]FileConfig) {
+	count := 0
+	joinedFiles := []FileConfig{}
+
+	for _, v := range *files {
+		if v.nest {
+			joinedFiles[count-1].codes = append(joinedFiles[count-1].codes, v.codes...)
+		} else {
+			joinedFiles = append(joinedFiles, v)
+			count++
+		}
+	}
+	*files = joinedFiles
+}
+
+func outputFile(body string, path string) {
 	file, err := os.Create(path)
 	if err != nil {
 		log.Fatal(err) //ファイルが開けなかったときエラー出力
@@ -94,4 +145,8 @@ func outputCode(body string, path string) {
 
 	defer file.Close()
 	file.Write(([]byte)(body))
+}
+
+func outputFileLines(bodyLines []string, path string) {
+	outputFile(strings.Join(bodyLines, "\n"), path)
 }
